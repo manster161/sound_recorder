b85d9c8fad6675d851362497ac2d3414f4b2c959
Måns Haegert <manster161@gmail.com>
2024-03-24 09:14:29 +0100

    Initiates block with Repo and Logger

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sound_recorder/sound_recorder_page.dart';
 import 'package:sound_recorder/sound_recorder_bloc.dart';
 import 'package:logger/logger.dart';
+import 'package:sound_recorder/sound_repository.dart';
 
 
 
@@ -41,7 +42,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: BlocProvider(create: (_) => SoundRecorderBloc() 
+      home: BlocProvider(create: (_) => SoundRecorderBloc(SoundRepository(), Logger()) 
       , child:  SoundRecorderPage()) 
     );
   }
