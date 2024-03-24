ccee9e262dfc0bcc02ce5cf6a479d0bc9b9c6f76
Måns Haegert <manster161@gmail.com>
2024-03-24 09:15:09 +0100

    Formats document

--- lib/sound_recorder_bloc.dart
+++ lib/sound_recorder_bloc.dart
@@ -6,33 +6,39 @@ import 'package:noise_meter/noise_meter.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'dart:async';
 
-class SoundRecorderBloc extends Bloc<SoundRecorderEvent, SoundRecorderState> {
-  SoundRecorderBloc() : super(SoundRecorderInitial());
+import 'package:sound_recorder/sound_repository.dart';
 
-  final Logger _logger = Logger();
+class SoundRecorderBloc extends Bloc<SoundRecorderEvent, SoundRecorderState> {
+  final Logger logger;
+  final SoundRepository soundRepository;
+
+  SoundRecorderBloc(this.soundRepository, this.logger)
+      : super(SoundRecorderInitial());
+
+  @override
+  SoundRecorderState get initialState => SoundRecorderInitial();
 
   NoiseReading? _latestReading;
   StreamSubscription<NoiseReading>? _noiseSubscription;
   NoiseMeter? noiseMeter;
 
   bool isRecording = false;
- 
+
   Future<bool> checkPermission() async => await Permission.microphone.isGranted;
   Future<void> requestPermission() async =>
       await Permission.microphone.request();
 
   void onData(NoiseReading event) {
-     _logger.d('On data called. Mean: ${event.meanDecibel}');
-    if(_latestReading == null){
+    logger.d('On data called. Mean: ${event.meanDecibel}');
+    if (_latestReading == null) {
       add(SoundRecorderDbLevelChange(event.meanDecibel));
       add(SoundRecorderPeakLevelChange(event.maxDecibel));
-    }
-    else{
-      if(event.meanDecibel != _latestReading!.meanDecibel){
+    } else {
+      if (event.meanDecibel != _latestReading!.meanDecibel) {
         add(SoundRecorderDbLevelChange(event.meanDecibel));
       }
-      if(event.maxDecibel < _latestReading!.maxDecibel){
-        _logger.i('Peak level changed');
+      if (event.maxDecibel < _latestReading!.maxDecibel) {
+        logger.i('Peak level changed');
         add(SoundRecorderPeakLevelChange(event.maxDecibel));
       }
     }
@@ -42,55 +48,42 @@ class SoundRecorderBloc extends Bloc<SoundRecorderEvent, SoundRecorderState> {
 
   void startRecording() async {
     Logger().i('Recorder Start');
-    
+
     if (await checkPermission()) {
-        isRecording = true;
-        noiseMeter ??= NoiseMeter();
-        _noiseSubscription = noiseMeter?.noise.listen(onData, onError: onError);
-      } else 
-      {
-       Logger().i('Requesting permission');
-       await requestPermission();
-      }
+      isRecording = true;
+      noiseMeter ??= NoiseMeter();
+      _noiseSubscription = noiseMeter?.noise.listen(onData, onError: onError);
+    } else {
+      Logger().i('Requesting permission');
+      await requestPermission();
+    }
   }
 
   void stopRecording() {
     _noiseSubscription?.cancel();
     isRecording = false;
-      _logger.i('Recorder Stop');
+    logger.i('Recorder Stop');
   }
 
   @override
   Stream<SoundRecorderState> mapEventToState(
     SoundRecorderEvent event,
   ) async* {
-    if (event is SoundRecorderStart) 
-    {
+    if (event is SoundRecorderStart) {
       startRecording();
       yield SoundRecorderRecording();
-    } 
-    else if (event is SoundRecorderStop) 
-    {
+    } else if (event is SoundRecorderStop) {
       stopRecording();
       yield SoundRecorderStopped();
-    } 
-    else if (event is SoundRecorderToggleEvent) 
-    {
-      if (isRecording) 
-      {
+    } else if (event is SoundRecorderToggleEvent) {
+      if (isRecording) {
         add(SoundRecorderStop());
-      } 
-      else 
-      {
+      } else {
         add(SoundRecorderStart());
       }
-    } 
-    else if (event is SoundRecorderDbLevelChange) 
-    {
+    } else if (event is SoundRecorderDbLevelChange) {
       yield SoundRecorderDbLevelChanged(event.currentDbLevel);
-    } 
-    else if (event is SoundRecorderPeakLevelChange) 
-    {
+    } else if (event is SoundRecorderPeakLevelChange) {
       yield SoundRecorderPeakLevelChanged(event.peakDbLevel);
     }
   }
