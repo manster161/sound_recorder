8594a28877c9a3d9c3780722f1a8f52215c7e652
Måns Haegert <manster161@gmail.com>
2024-04-03 11:34:52 +0200

    adds a settings dialog

--- lib/sound_recorder_page.dart
+++ lib/sound_recorder_page.dart
@@ -10,10 +10,26 @@ class SoundRecorderPage extends StatelessWidget {
 
   final Logger _logger = Logger(level: Level.info);
 
-  void onPressed() {
-    _logger.i('Setting pressed');
+  void onPressed(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Settings'),
+          content: const Text('This is the settings dialog.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,44 +37,47 @@ class SoundRecorderPage extends StatelessWidget {
         title: const Text('Sound Recorder'),
       ),
       body: Center(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Center(
-                      child:
-                      BlocBuilder<SoundRecorderBloc, SoundRecorderState>(
-                          builder: (context, state)  => 
-                               Text('Mean db level: ${state.currentDbLevel.toString()}')
-                          )),
-                ],
-              ),
-              Row(
-                children: [
-                  Center(
-                      child: 
-                      BlocBuilder<SoundRecorderBloc, SoundRecorderState>(
-                          builder: (context, state) => 
-                              Text('Peak db level: ${state.peakDbLevel.toString()}')
-                          )),
-                ],
-              ),
-            ],
-          ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Center(
+                  child: BlocBuilder<SoundRecorderBloc, SoundRecorderState>(
+                    builder: (context, state) => 
+                      Text('Mean db level: ${state.currentDbLevel.toString()}')
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Center(
+                  child: BlocBuilder<SoundRecorderBloc, SoundRecorderState>(
+                    builder: (context, state) => 
+                      Text('Peak db level: ${state.peakDbLevel.toString()}')
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
+      ),
       floatingActionButton: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           FloatingActionButton(
-              child: BlocBuilder<SoundRecorderBloc, SoundRecorderState>(
-                builder: (context, state) => Text(state.recordButtonText),
-              ),
-              onPressed: () => BlocProvider.of<SoundRecorderBloc>(context)
-                  .add(const SoundRecorderToggleEvent(0.0, 0.0))),
+            child: BlocBuilder<SoundRecorderBloc, SoundRecorderState>(
+              builder: (context, state) => Text(state.recordButtonText),
+            ),
+            onPressed: () => BlocProvider.of<SoundRecorderBloc>(context)
+              .add(const SoundRecorderToggleEvent(0.0, 0.0)),
+          ),
           const SizedBox(height: 4),
           FloatingActionButton(
-              child: const Text('Settings'), onPressed: () => onPressed()),
+            child: const Text('Settings'),
+            onPressed: () => onPressed(context),
+          ),
         ],
       ),
     );
